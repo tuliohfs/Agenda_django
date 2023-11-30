87474582f2200c699c4642720e3de02a90358993
tuliohfs <tulio.henrique@liv.local>
2023-11-30 14:37:55 -0300

    Commit do html projeto

--- core/models.py
+++ core/models.py
@@ -4,8 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Evento(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True, verbose_name='DATA DA CRIAÇÃO')
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime("%d/%m/%Y %H:%M")
--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
 
-# Create your views here.
+def index(request):
+    return redirect('/Agenda/')
+
+
+def lista_eventos(request):
+    
+    usuario = request.user
+    # 'Evento.objects.all()' está pegando todos os objetos do modelo Evento.
+    evento_instance = Evento.objects.all()
+    # 'dados' é um dicionário que contém os dados que você quer passar para o template.
+    dados = {'Evento_instance': evento_instance}
+    # 'render' está renderizando o template 'agenda.html' e passando o dicionário 'dados' para ele.
+    return render(request, 'agenda.html', dados)
