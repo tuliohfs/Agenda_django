010e2efa7840be4770689095b90cbd045a8ad810
Túlio Silva <84759095+tuliohfs@users.noreply.github.com>
2023-12-11 15:35:15 -0300

    Commit de criação de eventos dentro do site

--- agenda_django/urls.py
+++ agenda_django/urls.py
@@ -5,8 +5,11 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Agenda/', views.lista_eventos),
+    path('Agenda/evento/', views.evento),
+    path('Agenda/evento/submit', views.submit_evento),
     path('', views.index),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
-    path('logout/', views.logout_user)
+    path('logout/', views.logout_user),
+
 ]
--- core/views.py
+++ core/views.py
@@ -33,8 +33,22 @@ def submit_login(request):
         else:
             messages.error(request, 'Usuário ou senha inválido')
     return redirect('/')
-    
 
 def logout_user(request):
     logout(request)
     return redirect('/')
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, "evento.html")
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
+    return redirect('/')
+    
